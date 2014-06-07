6c183cee3605984468c45f148dcbe15c4669034d
khz@tzi.org <khz@tzi.org>
2014-06-07 23:40:42 +0200

    added a better docstring for init_controllers()

--- tornado_server.py
+++ tornado_server.py
@@ -19,6 +19,48 @@ copow_handlers = #APPNAME.config.routes.handlers
 copow_settings = #APPNAME.config.settings.webserver
 
 def init_controllers(app):
+	"""
+		automatically adds RESTful routes for eacg controller in controllers/
+		which is named according to the convention: name_controller
+
+		Automatically calls the right cotroller->CRUDMethod.
+        According to this mapping table:
+        **********
+        * ACTION:
+        **********
+        in General:
+        HTTP    Method      CRUD Action Description
+        -----------------------------------------------
+        POST    CREATE      Create a new resource
+        GET     RETRIEVE    Retrieve a representation of a resource
+        PUT     UPDATE      Update a resource
+        DELETE  DELETE      Delete a resource
+
+        PUT and DELETE must be handled with a POST request and an
+        addiotional HTTP Parameter: REQUEST_TYPE
+        set to PUT or DELETE accordingly.
+
+        Meaning a call to domain:port/controller/([someting]+)
+        Where something is usually an ID
+        HTTP get        => will call controller.show(something)
+        HTTP POST       => will call controller.create(something)
+        HTTP PUT        => will call controller.edit(something)
+        HTTP DELETE     => will call controller.delete(something)
+        and a call to domain:port/controller/
+        HTTP get        => will call controller.list()
+        HTTP POST       => will call Nothing, yet.
+        HTTP PUT        => will call controller.replace_all() [empty by default]
+        HTTP DELETE     => will call controller.delete_all()
+
+        ***********
+        * FORMAT:
+        ***********
+        Also all reuests can have an Accept: HTTP header field which must be parsed by the
+        controller itself.
+        Example:    Accept:      text/json
+
+        
+	"""
 	path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "controllers")
 	print("path ffor controllers: ", path)
 	handler_list = []
